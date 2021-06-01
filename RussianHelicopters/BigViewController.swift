706968c04f56336266b57c347964af1143ea9f24
S.Kornev <post.kornev@gmail.com>
2021-06-01 16:11:00 +0300

    Some changes (resizing video)

--- RussianHelicopters/BigViewController.swift
+++ RussianHelicopters/BigViewController.swift
@@ -9,8 +9,6 @@ import UIKit
 import AVKit
 
 class BigViewController: UIViewController {
-
- //   let videos: [Video] = Video.allVideos()
     
     let videoPreviewLooper = VideoLooperView(videos: Video.localVideos())
     
@@ -20,9 +18,8 @@ class BigViewController: UIViewController {
         super.viewDidLoad()
 
         view.addSubview(videoPreviewLooper)
- //       setupCollectionView()
         
-        videoPreviewLooper.frame = CGRect(x: view.bounds.width - 150 - 16, y: view.bounds.height - 100 - 16, width: 150, height: 100)
+        videoPreviewLooper.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
         videoPreviewLooper.backgroundColor = .black
     }
     
@@ -36,44 +33,6 @@ class BigViewController: UIViewController {
         videoPreviewLooper.pause()
     }
     
-//    func setupCollectionView() {
-//        collectionView.delegate = self
-//        collectionView.dataSource = self
-//        collectionView.register(GalleryCollectionViewCell.self, forCellWithReuseIdentifier: GalleryCollectionViewCell.reuseId)
-//
-//        // настраиваем величины отступов и размеров ячеек в collection view
-//        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-//        layout.minimumInteritemSpacing = Constants.galleryMinimumInteritemSpacing
-//        collectionView.contentInset = UIEdgeInsets(top: Constants.topDistanceToView, left: Constants.leftDistanceToView, bottom: Constants.bottomDistanceToView, right: Constants.rightDistanceToView)
-//        layout.itemSize = CGSize(width: Constants.galleryItemWidth , height: Constants.galleryItemHeight)
-//    }
 }
-
-//extension BigViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-//
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return videos.count
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GalleryCollectionViewCell.reuseId, for: indexPath) as! GalleryCollectionViewCell
-//        cell.video = videos[indexPath.row]
-//        return cell
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        let video = videos[indexPath.row]
-//
-//        let videoURL = video.url
-//        let player = AVPlayer(url: videoURL)
-//
-//        let playerViewController = AVPlayerViewController()
-//        playerViewController.player = player
-//
-//        present(playerViewController, animated: true) {
-//            player.play()
-//        }
-//    }
-//}
 
 
